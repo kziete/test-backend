bb3c39b0abffb6c32b910514bb0f19cddad2d73e
kziete <sdiaz@talana.com>
2021-07-21 14:35:21 -0400

    format

--- adventure/tests/test_01_entities.py
+++ adventure/tests/test_01_entities.py
@@ -4,6 +4,7 @@ import pytest
 
 from adventure import models
 
+
 @pytest.fixture
 def car():
     return models.VehicleType(max_capacity=5)
@@ -11,33 +12,24 @@ def car():
 
 class TestVehicle:
     def test_capacity_greater_than_passengers(self, car):
-        vehicle = models.Vehicle(
-            vehicle_type=car, passengers=2
-        )
+        vehicle = models.Vehicle(vehicle_type=car, passengers=2)
         assert vehicle.can_start()
 
     def test_vehicle_overload(self, car):
-        vehicle = models.Vehicle(
-            vehicle_type=car, passengers=10
-        )
+        vehicle = models.Vehicle(vehicle_type=car, passengers=10)
         assert not vehicle.can_start()
 
     @pytest.mark.skip
     def test_vehicle_distribution(self, car):
-        vehicle = models.Vehicle(
-            vehicle_type=car, passengers=2
-        )
-        distribution_expected = [
-            [True, True],
-            [True, False],
-        ]
+        vehicle = models.Vehicle(vehicle_type=car, passengers=2)
+        distribution_expected = [[True, True], [True, False]]
         assert vehicle.get_distribution() == distribution_expected
 
 
 @pytest.mark.skip
 class TestJourney:
     # TODO: implement "is_finished" method
-    # a finished journey depends on the end value 
+    # a finished journey depends on the end value
     def test_is_finished(self):
         journey = models.Journey(start=date.today(), end=date.today())
         assert journey.is_finished()
